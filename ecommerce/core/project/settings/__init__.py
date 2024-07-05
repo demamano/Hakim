bd63fa57ce913dbf0ebd556ba4dce913b44f7177
Dema Amano <demaamano@gmail.com>
2024-07-05 16:57:49 +0300

    chore: Update project file paths and add makeFile target and recipe

--- ecommerce/core/project/settings/__init__.py
+++ ecommerce/core/project/settings/__init__.py
@@ -2,3 +2,17 @@ import os.path
 from pathlib import Path
 from split_settings.tools import optional, include
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+ENVVAR_SETTINGS_PREFIX = 'CORESETTINGS_'
+
+LOCAL_SETTINGS_PATH = os.getenv(f'{ENVVAR_SETTINGS_PREFIX}LOCAL_SETTINGS_PATH')
+
+if not LOCAL_SETTINGS_PATH:
+    LOCAL_SETTINGS_PATH = 'local/settings.dev.py'
+
+if not os.path.isabs(LOCAL_SETTINGS_PATH):
+    LOCAL_SETTINGS_PATH = str(BASE_DIR / LOCAL_SETTINGS_PATH)
+
+include(
+    'base.py',
+    optional(LOCAL_SETTINGS_PATH),
+)
